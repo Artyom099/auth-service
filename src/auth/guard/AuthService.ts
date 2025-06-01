70f3886a18f21f45f434e636c11e0af7628b5945
Artyom Golubev <artgolubev@bk.ru>
2025-06-02 00:12:21 +0300

    wip

--- src/auth/guard/AuthService.ts
+++ src/auth/guard/AuthService.ts
@@ -1,12 +1,52 @@
+import { Injectable } from '@nestjs/common';
 import { EntityManager } from 'typeorm';
 
-import { AccessObject, AccessObjectAction, Right, Role } from '../../libs/db/entity';
+import { AccessObject, AccessObjectAction, Right, Role, RoleHierarchy, User, UserRole } from '../../libs/db/entity';
 
+@Injectable()
 export class AuthService {
   constructor(private readonly manager: EntityManager) {}
 
-  public async getUserRoles(): Promise<string[]> {
-    // todo 1
+  public async getUserRoles(userId: string): Promise<string[]> {
+    const rolesCte = [
+      this.manager
+        .createQueryBuilder()
+        .select('r.name', 'name')
+        .addSelect('h.parentName', 'parentName')
+        .from(Role, 'r')
+        .leftJoin(RoleHierarchy, 'h', 'h.name = r.name')
+        .leftJoin(UserRole, 'ur', 'ur.roleName = r.name')
+        .leftJoin(User, 'u', 'u.id = ur.userId')
+        .where('u.id = :userId'),
+
+      this.manager
+        .createQueryBuilder()
+        .select('r.name', 'name')
+        .addSelect('h.parentName', 'parentName')
+        .from(Role, 'r')
+        .leftJoin(RoleHierarchy, 'h', 'h.name = r.name')
+        .innerJoin('roles', 'roles', '"roles"."parentName" = r.name'),
+    ]
+      .map((qb) => qb.getQuery())
+      .join(' union all ');
+
+    const qb = this.manager
+      .createQueryBuilder()
+      .select('roles.name', 'name')
+      .from('roles', 'roles')
+      .addCommonTableExpression(rolesCte, 'roles', {
+        recursive: true,
+        columnNames: ['name', 'parentName'],
+      })
+      .setParameters({ userId });
+
+    console.log({ sql: qb.getQueryAndParameters() });
+
+    const userRoles = await qb.getRawMany<{ name: string; parentName: string }>();
+    console.log({ userRoles });
+
+    const roles = userRoles.map((role) => role.name);
+
     return ['admin'];
   }
 
@@ -20,7 +60,7 @@ export class AuthService {
       .innerJoin(Right, 'r', 'role.name = r.roleName')
       .innerJoin(AccessObjectAction, 'aoa', 'aoa.actionName = r.actionName')
       .innerJoin(AccessObject, 'ao', 'ao.name = aoa.objectName')
-      // todo 2
+      // todo 2 - add ActionApi, Api
       .innerJoin('ActionApi', 'aa', 'aa.actionName = r.actionName')
       .innerJoin('Api', 'api', 'api.name = aa.apiName')
       .where('role.name in (:...roles)')
